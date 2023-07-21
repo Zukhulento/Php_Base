77528ef17275685ae365f45815cb50c66bfe3d32
Zukhulento <luis.matus.jr@gmail.com>
2023-07-20 19:28:25 -0600

    Contacts table

---
+++ sql/setup.sql
@@ -0,0 +1,13 @@
+DROP DATABASE contacts_app;
+
+CREATE DATABASE contacts_app;
+
+USE contacts_app;
+
+CREATE TABLE contacts (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255),
+  phone_number VARCHAR(255) 
+);
+
+INSERT INTO contacts (name, phone_number) VALUES ("Pepe","1234567890");
